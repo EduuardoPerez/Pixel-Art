3c0e5e2b8e9638a3bf7ffcbb9c58064525dc05df
Eduardo Perez <edujpp1@gmail.com>
2019-08-27 09:44:20 -0300

    Al hacer clic en un color de la paleta se cambia el color del indicador de color. Si se ejecuta en la consola del navegador ese trozo de código funciona. Pero, desde el codigo como tal no está funcionando

--- js/pixel-art.js
+++ js/pixel-art.js
@@ -1,61 +1,66 @@
-var nombreColores = ['White', 'LightYellow',
-  'LemonChiffon', 'LightGoldenrodYellow', 'PapayaWhip', 'Moccasin', 'PeachPuff', 'PaleGoldenrod', 'Bisque', 'NavajoWhite', 'Wheat', 'BurlyWood', 'Tan',
-  'Khaki', 'Yellow', 'Gold', 'Orange', 'DarkOrange', 'OrangeRed', 'Tomato', 'Coral', 'DarkSalmon', 'LightSalmon', 'LightCoral', 'Salmon', 'PaleVioletRed',
-  'Pink', 'LightPink', 'HotPink', 'DeepPink', 'MediumVioletRed', 'Crimson', 'Red', 'FireBrick', 'DarkRed', 'Maroon',
-  'Brown', 'Sienna', 'SaddleBrown', 'IndianRed', 'RosyBrown',
-  'SandyBrown', 'Goldenrod', 'DarkGoldenrod', 'Peru',
-  'Chocolate', 'DarkKhaki', 'DarkSeaGreen', 'MediumAquaMarine',
-  'MediumSeaGreen', 'SeaGreen', 'ForestGreen', 'Green', 'DarkGreen', 'OliveDrab', 'Olive', 'DarkOliveGreen', 'YellowGreen', 'LawnGreen',
-  'Chartreuse', 'GreenYellow', 'Lime', 'SpringGreen', 'LimeGreen',
-  'LightGreen', 'PaleGreen', 'PaleTurquoise',
-  'AquaMarine', 'Cyan', 'Turquoise', 'MediumTurquoise', 'DarkTurquoise', 'DeepSkyBlue',
-  'LightSeaGreen', 'CadetBlue', 'DarkCyan', 'Teal', 'Steelblue', 'LightSteelBlue', 'Honeydew', 'LightCyan',
-  'PowderBlue', 'LightBlue', 'SkyBlue', 'LightSkyBlue',
-  'DodgerBlue', 'CornflowerBlue', 'RoyalBlue', 'SlateBlue',
-  'MediumSlateBlue', 'DarkSlateBlue', 'Indigo', 'Purple', 'DarkMagenta', 'Blue',
-  'MediumBlue', 'DarkBlue', 'Navy', 'Thistle',
-  'Plum', 'Violet', 'Orchid', 'DarkOrchid', 'Fuchsia', 'Magenta', 'MediumOrchid',
-  'BlueViolet', 'DarkViolet', 'DarkOrchid',
-  'MediumPurple', 'Lavender', 'Gainsboro', 'LightGray', 'Silver', 'DarkGray', 'Gray',
-  'DimGray', 'LightSlateGray', 'DarkSlateGray', 'Black'
-];
-
-// Variable para guardar el elemento 'color-personalizado'
-// Es decir, el que se elige con la rueda de color.
-var colorPersonalizado = document.getElementById('color-personalizado');
-var paleta = $('#paleta');
-var grillaPixeles = $('#grilla-pixeles');
-
-colorPersonalizado.addEventListener('change', 
-  (function() {
-    // Se guarda el color de la rueda en colorActual
-    colorActual = colorPersonalizado.value;
-    // Completar para que cambie el indicador-de-color al colorActual
-
-
-  })
-);
-
-// Generar la paleta de colores
-function generarPaleta() {
-
-  nombreColores.forEach(color => {
-    $(`<div></div>`, {
-      class:'color-paleta',
-      style:`background-color:${color}`
-    }).appendTo(paleta);
-  });
-}
-
-// Generar la grilla
-function generarLienzo() {
-  
-  for (let i=0; i<1750; i++) {
-    $(`<div></div>`).appendTo(grillaPixeles);
-  }
-}
-
 $(function() {
+  var nombreColores = ['White', 'LightYellow',
+    'LemonChiffon', 'LightGoldenrodYellow', 'PapayaWhip', 'Moccasin', 'PeachPuff', 'PaleGoldenrod', 'Bisque', 'NavajoWhite', 'Wheat', 'BurlyWood', 'Tan',
+    'Khaki', 'Yellow', 'Gold', 'Orange', 'DarkOrange', 'OrangeRed', 'Tomato', 'Coral', 'DarkSalmon', 'LightSalmon', 'LightCoral', 'Salmon', 'PaleVioletRed',
+    'Pink', 'LightPink', 'HotPink', 'DeepPink', 'MediumVioletRed', 'Crimson', 'Red', 'FireBrick', 'DarkRed', 'Maroon',
+    'Brown', 'Sienna', 'SaddleBrown', 'IndianRed', 'RosyBrown',
+    'SandyBrown', 'Goldenrod', 'DarkGoldenrod', 'Peru',
+    'Chocolate', 'DarkKhaki', 'DarkSeaGreen', 'MediumAquaMarine',
+    'MediumSeaGreen', 'SeaGreen', 'ForestGreen', 'Green', 'DarkGreen', 'OliveDrab', 'Olive', 'DarkOliveGreen', 'YellowGreen', 'LawnGreen',
+    'Chartreuse', 'GreenYellow', 'Lime', 'SpringGreen', 'LimeGreen',
+    'LightGreen', 'PaleGreen', 'PaleTurquoise',
+    'AquaMarine', 'Cyan', 'Turquoise', 'MediumTurquoise', 'DarkTurquoise', 'DeepSkyBlue',
+    'LightSeaGreen', 'CadetBlue', 'DarkCyan', 'Teal', 'Steelblue', 'LightSteelBlue', 'Honeydew', 'LightCyan',
+    'PowderBlue', 'LightBlue', 'SkyBlue', 'LightSkyBlue',
+    'DodgerBlue', 'CornflowerBlue', 'RoyalBlue', 'SlateBlue',
+    'MediumSlateBlue', 'DarkSlateBlue', 'Indigo', 'Purple', 'DarkMagenta', 'Blue',
+    'MediumBlue', 'DarkBlue', 'Navy', 'Thistle',
+    'Plum', 'Violet', 'Orchid', 'DarkOrchid', 'Fuchsia', 'Magenta', 'MediumOrchid',
+    'BlueViolet', 'DarkViolet', 'DarkOrchid',
+    'MediumPurple', 'Lavender', 'Gainsboro', 'LightGray', 'Silver', 'DarkGray', 'Gray',
+    'DimGray', 'LightSlateGray', 'DarkSlateGray', 'Black'
+  ];
+
+  // Variable para guardar el elemento 'color-personalizado'
+  // Es decir, el que se elige con la rueda de color.
+  var colorPersonalizado = document.getElementById('color-personalizado');
+  var paleta = $('#paleta');
+  var grillaPixeles = $('#grilla-pixeles');
+
+  colorPersonalizado.addEventListener('change', 
+    (function() {
+      // Se guarda el color de la rueda en colorActual
+      colorActual = colorPersonalizado.value;
+      // Completar para que cambie el indicador-de-color al colorActual
+
+
+    })
+  );
+
+  // Generar la paleta de colores
+  function generarPaleta() {
+    nombreColores.forEach(color => {
+      $(`<div></div>`, {
+        class:'color-paleta',
+        style:`background-color:${color}`
+      }).appendTo(paleta);
+    });
+  };
+
+  // Generar la grilla
+  function generarLienzo() {
+    for (let i=0; i<1750; i++) {
+      $(`<div></div>`).appendTo(grillaPixeles);
+    }
+  };
+
+  // Cambia el color del indicador de color según el que se escoja en la paleta
+  let colorPaleta = $('.color-paleta');
+  colorPaleta.click(function(){
+    let colorSeleccionado = $(this).css('background-color');
+    $('#indicador-de-color').css('background-color', colorSeleccionado);
+  });
+
   generarPaleta();
   generarLienzo();
 });
